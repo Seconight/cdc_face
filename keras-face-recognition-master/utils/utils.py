c12e353fcde6c32dcd31a551c1730cef08cd47c2
Seconight <614300076@qq.com>
2021-01-13 10:56:56 +0800

    update

--- keras-face-recognition-master/utils/utils.py
+++ keras-face-recognition-master/utils/utils.py
@@ -4,7 +4,7 @@ import numpy as np
 import cv2
 import math
 import matplotlib.pyplot as plt
-#test
+
 #计算原始输入图像每一次缩放的比例（构建图像金字塔）
 def calculateScales(img):   #img为传入的图像
     copy_img = img.copy()   #复制图像
@@ -37,7 +37,7 @@ def calculateScales(img):   #img为传入的图像
     return scales   #返回缩放比例的列表
 
 
-#对pnet处理后的结果进行处理
+#对Pnet处理后的结果进行处理
 def detect_face_12net(cls_prob,roi,out_side,scale,width,height,threshold):
     #原始权重是caffe上训练的，yx轴是交换的，这里将y和x进行转换
     cls_prob = np.swapaxes(cls_prob, 0, 1)
@@ -62,11 +62,12 @@ def detect_face_12net(cls_prob,roi,out_side,scale,width,height,threshold):
     dx3 = roi[2][x,y]
     dx4 = roi[3][x,y]
     score = np.array([cls_prob[x,y]]).T
-    offset = np.array([dx1,dx2,dx3,dx4]).T
-    boundingbox = boundingbox + offset*12.0*scale
-    rectangles = np.concatenate((boundingbox,score),axis=1)
+    offset = np.array([dx1,dx2,dx3,dx4]).T  #得到偏移量
+    boundingbox = boundingbox + offset*12.0*scale   #变换到原图
 
-    rectangles = rect2square(rectangles)
+    #进行堆叠将矩形变换成正方形方便Onet处理
+    rectangles = np.concatenate((boundingbox,score),axis=1)
+    rectangles = rect2square(rectangles)    
     pick = []
     #对坐标进行限制不能超出图片范围
     for i in range(len(rectangles)):
@@ -78,9 +79,9 @@ def detect_face_12net(cls_prob,roi,out_side,scale,width,height,threshold):
         if x2>x1 and y2>y1:
             pick.append([x1,y1,x2,y2,sc])
     return NMS(pick,0.3)    #进行非极大值抑制剔除掉重合率较高的框
-#-----------------------------#
-#   将长方形调整为正方形
-#-----------------------------#
+
+
+#将长方形调整为正方形
 def rect2square(rectangles):
     w = rectangles[:,2] - rectangles[:,0]
     h = rectangles[:,3] - rectangles[:,1]
@@ -90,11 +91,12 @@ def rect2square(rectangles):
     rectangles[:,2:4] = rectangles[:,0:2] + np.repeat([l], 2, axis = 0).T 
     return rectangles
 
+
 #非极大抑制剔除掉重合率较高的框
 def NMS(rectangles,threshold):
     if len(rectangles)==0:
         return rectangles
-    boxes = np.array(rectangles)    #将矩形序列创建为数组形式
+    boxes = np.array(rectangles)    #转化成numpy数组
     #取出左上右下两个点坐标
     x1 = boxes[:,0] #取boxes每个元素的第一项
     y1 = boxes[:,1]
@@ -125,38 +127,39 @@ def NMS(rectangles,threshold):
     return result_rectangle
 
 
-#-------------------------------------#
-#   对pnet处理后的结果进行处理
-#-------------------------------------#
+#对Rnet处理后的结果进行处理
 def filter_face_24net(cls_prob,roi,rectangles,width,height,threshold):
-    
+    #筛选出人脸置信度大于门限的
     prob = cls_prob[:,1]
     pick = np.where(prob>=threshold)
-    
+    #转化成numpy数组
     rectangles = np.array(rectangles)
-
+    
+    #原始人脸框位置
     x1  = rectangles[pick,0]
     y1  = rectangles[pick,1]
     x2  = rectangles[pick,2]
     y2  = rectangles[pick,3]
-    
+    #这一步在干啥还不知道！！！
     sc  = np.array([prob[pick]]).T
-
+    #对原始人脸框进行调整的参数
     dx1 = roi[pick,0]
     dx2 = roi[pick,1]
     dx3 = roi[pick,2]
     dx4 = roi[pick,3]
-
+    #原始人脸框宽和高
     w   = x2-x1
     h   = y2-y1
-
+    #调整后的人脸框的位置
     x1  = np.array([(x1+dx1*w)[0]]).T
     y1  = np.array([(y1+dx2*h)[0]]).T
     x2  = np.array([(x2+dx3*w)[0]]).T
     y2  = np.array([(y2+dx4*h)[0]]).T
-
+    #进行堆叠并转换成正方形方便Onet处理
     rectangles = np.concatenate((x1,y1,x2,y2,sc),axis=1)
     rectangles = rect2square(rectangles)
+
+    #对坐标进行限制不能超出图片范围
     pick = []
     for i in range(len(rectangles)):
         x1 = int(max(0     ,rectangles[i][0]))
@@ -166,31 +169,32 @@ def filter_face_24net(cls_prob,roi,rectangles,width,height,threshold):
         sc = rectangles[i][4]
         if x2>x1 and y2>y1:
             pick.append([x1,y1,x2,y2,sc])
-    return NMS(pick,0.3)
-#-------------------------------------#
-#   对onet处理后的结果进行处理
-#-------------------------------------#
+    return NMS(pick,0.3)    #非极大值抑制
+
+
+#对Onet处理后的结果进行处理
 def filter_face_48net(cls_prob,roi,pts,rectangles,width,height,threshold):
-    
+    #筛选出人脸置信度大于门限的框
     prob = cls_prob[:,1]
     pick = np.where(prob>=threshold)
-    rectangles = np.array(rectangles)
-
+    rectangles = np.array(rectangles)   #转化成numpy数组
+    #原始人脸框位置
     x1  = rectangles[pick,0]
     y1  = rectangles[pick,1]
     x2  = rectangles[pick,2]
     y2  = rectangles[pick,3]
 
     sc  = np.array([prob[pick]]).T
-
+    #对原始人脸框进行调整的参数
     dx1 = roi[pick,0]
     dx2 = roi[pick,1]
     dx3 = roi[pick,2]
     dx4 = roi[pick,3]
-
+    #原始人脸框宽和高
     w   = x2-x1
     h   = y2-y1
-
+    
+    #人脸的五个特征点位置
     pts0= np.array([(w*pts[pick,0]+x1)[0]]).T
     pts1= np.array([(h*pts[pick,5]+y1)[0]]).T
 
@@ -206,13 +210,15 @@ def filter_face_48net(cls_prob,roi,pts,rectangles,width,height,threshold):
     pts8= np.array([(w*pts[pick,4]+x1)[0]]).T
     pts9= np.array([(h*pts[pick,9]+y1)[0]]).T
 
+    #调整后的人脸框的位置
     x1  = np.array([(x1+dx1*w)[0]]).T
     y1  = np.array([(y1+dx2*h)[0]]).T
     x2  = np.array([(x2+dx3*w)[0]]).T
     y2  = np.array([(y2+dx4*h)[0]]).T
-
+    #进行堆叠
     rectangles=np.concatenate((x1,y1,x2,y2,sc,pts0,pts1,pts2,pts3,pts4,pts5,pts6,pts7,pts8,pts9),axis=1)
 
+    #对坐标进行限制不能超出图片范围
     pick = []
     for i in range(len(rectangles)):
         x1 = int(max(0     ,rectangles[i][0]))
@@ -222,7 +228,7 @@ def filter_face_48net(cls_prob,roi,pts,rectangles,width,height,threshold):
         if x2>x1 and y2>y1:
             pick.append([x1,y1,x2,y2,rectangles[i][4],
                  rectangles[i][5],rectangles[i][6],rectangles[i][7],rectangles[i][8],rectangles[i][9],rectangles[i][10],rectangles[i][11],rectangles[i][12],rectangles[i][13],rectangles[i][14]])
-    return NMS(pick,0.3)
+    return NMS(pick,0.3)    #非极大值抑制
 
 #-------------------------------------#
 #   人脸对齐
