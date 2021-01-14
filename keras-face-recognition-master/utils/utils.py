53326f200eade74f92b8c44eb572b75d24cc281d
Seconight <614300076@qq.com>
2021-01-14 16:39:34 +0800

    update

--- keras-face-recognition-master/utils/utils.py
+++ keras-face-recognition-master/utils/utils.py
@@ -233,24 +233,24 @@ def filter_face_48net(cls_prob,roi,pts,rectangles,width,height,threshold):
 
 #人脸对齐
 def Alignment_1(img,landmark):
-
+    #计算眼睛连线相对于水平线的倾斜角
     if landmark.shape[0]==68:
         x = landmark[36,0] - landmark[45,0]
         y = landmark[36,1] - landmark[45,1]
     elif landmark.shape[0]==5:
         x = landmark[0,0] - landmark[1,0]
         y = landmark[0,1] - landmark[1,1]
-
+    #计算弧度值
     if x==0:
         angle = 0
     else: 
         angle = math.atan(y/x)*180/math.pi
-
+    #求出旋转中心位置
     center = (img.shape[1]//2, img.shape[0]//2)
-
+    #进行矫正
     RotationMatrix = cv2.getRotationMatrix2D(center, angle, 1)
     new_img = cv2.warpAffine(img,RotationMatrix,(img.shape[1],img.shape[0])) 
-
+    #计算对齐过后五个特征点的位置
     RotationMatrix = np.array(RotationMatrix)
     new_landmark = []
     for i in range(landmark.shape[0]):
@@ -261,42 +261,6 @@ def Alignment_1(img,landmark):
 
     new_landmark = np.array(new_landmark)
 
-    return new_img, new_landmark
-
-def Alignment_2(img,std_landmark,landmark):
-    def Transformation(std_landmark,landmark):
-        std_landmark = np.matrix(std_landmark).astype(np.float64)
-        landmark = np.matrix(landmark).astype(np.float64)
-
-        c1 = np.mean(std_landmark, axis=0)
-        c2 = np.mean(landmark, axis=0)
-        std_landmark -= c1
-        landmark -= c2
-
-        s1 = np.std(std_landmark)
-        s2 = np.std(landmark)
-        std_landmark /= s1
-        landmark /= s2 
-
-        U, S, Vt = np.linalg.svd(std_landmark.T * landmark)
-        R = (U * Vt).T
-
-        return np.vstack([np.hstack(((s2 / s1) * R, c2.T - (s2 / s1) * R * c1.T)),np.matrix([0., 0., 1.])])
-
-    Trans_Matrix = Transformation(std_landmark,landmark) # Shape: 3 * 3
-    Trans_Matrix = Trans_Matrix[:2]
-    Trans_Matrix = cv2.invertAffineTransform(Trans_Matrix)
-    new_img = cv2.warpAffine(img,Trans_Matrix,(img.shape[1],img.shape[0]))
-
-    Trans_Matrix = np.array(Trans_Matrix)
-    new_landmark = []
-    for i in range(landmark.shape[0]):
-        pts = []    
-        pts.append(Trans_Matrix[0,0]*landmark[i,0]+Trans_Matrix[0,1]*landmark[i,1]+Trans_Matrix[0,2])
-        pts.append(Trans_Matrix[1,0]*landmark[i,0]+Trans_Matrix[1,1]*landmark[i,1]+Trans_Matrix[1,2])
-        new_landmark.append(pts)
-
-    new_landmark = np.array(new_landmark)
     return new_img, new_landmark
 
 
